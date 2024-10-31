7b733b0778f6ed3992021ea04a8d51153c9cda86
Jacob Penner <japenner@gmail.com>
2024-10-31 09:16:47 -0400

    add script to open all requested reviews

---
+++ zsh/scripts/open_pr_review_requests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Originally written for MacOS
+gh search prs --review-requested=@me --state=open --archived=false --json url,isDraft --limit=100 |
+  jq -r '.[] | select(.isDraft == false) | .url' | {
+  read -r first_url
+  open -na "Google Chrome" --args --new-window "$first_url"
+  while read -r url; do
+    open -na "Google Chrome" --args "$url"
+  done
+}
