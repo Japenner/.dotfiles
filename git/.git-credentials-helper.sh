44186c4bd3f205635944ff5be54fa6fa08490e25
Jacob Penner <japenner@gmail.com>
2024-11-18 15:02:17 -0500

    [AUTOMATED] feat: misc changes to dotfiles - 2024-11-18 15:02:17

--- git/.git-credentials-helper.sh
+++ git/.git-credentials-helper.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 case "$GIT_PREFIX" in
-    ~/Repos/personal/*)
-        echo "username=japenner"
-        echo "password=$GITHUB_PAT_PERSONAL"
-        ;;
-    ~/Repos/ad_hoc/va/*)
-        echo "username=pennja"
-        echo "password=$GITHUB_PAT_AD_HOC"
-        ;;
-    *)
-        echo "No matching credentials"
-        ;;
+~/repos/personal/*)
+    echo "username=japenner"
+    echo "password=$GITHUB_PAT_PERSONAL"
+    ;;
+~/repos/ad_hoc/va/*)
+    echo "username=pennja"
+    echo "password=$GITHUB_PAT_AD_HOC"
+    ;;
+*)
+    echo "No matching credentials"
+    ;;
 esac
