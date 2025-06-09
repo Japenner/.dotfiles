28170b1383f7f852c3503fd29f09c940a39babcf
Jacob Penner <japenner@gmail.com>
2025-06-09 13:23:14 -0400

    [AUTOMATED] feat: misc changes to dotfiles - 2025-06-09 13:23:14

--- zsh/functions/github.sh
+++ zsh/functions/github.sh
@@ -4,6 +4,51 @@
 open_cmd="open"
 command -v open >/dev/null 2>&1 || open_cmd="xdg-open"
 
+############ UTILITY FUNCTIONS ###########
+
+# Get the GitHub repository URL in HTTPS format, removing any custom SSH host aliases
+github_project_root() {
+  local file_path=${1:-$(pwd)}
+  local url=$(git -C "$file_path" config remote.origin.url)
+
+  # Ensure the URL is non-empty
+  if [[ -z "$url" ]]; then
+    echo "Error: Unable to determine GitHub project root." >&2
+    return 1
+  fi
+
+  # Escape special characters and transform URL
+  echo "$url" |
+    sed -E 's|git@([^:]+):(.+)|https://\1/\2|' | # Convert SSH to HTTPS
+    sed -E 's|\.git$||' |                        # Remove `.git` suffix
+    sed -E 's|github\.com-work|github.com|' |    # Normalize host alias
+    sed -E 's|github\.com-personal|github.com|'  # Normalize host alias
+}
+
+# Get the default branch based on the remote's HEAD
+github_default_branch() {
+  local file_path=${1:-$(pwd)}
+  git -C "$file_path" symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'
+}
+
+# Get the current branch for a given path (default: current directory)
+github_current_branch() {
+  local file_path=${1:-$(pwd)}
+  git -C "$file_path" symbolic-ref --short HEAD
+}
+
+# Update the current branch by rebasing it on top of the default branch
+github_update_current_branch() {
+  local file_path=${1:-$(pwd)}
+  local default_branch=$(github_default_branch "$file_path")
+
+  git -C "$file_path" fetch origin
+  git -C "$file_path" pull origin "$default_branch" --rebase
+  git -C "$file_path" push origin "$(github_current_branch "$file_path")" --force --no-verify
+}
+
+##########################################
+
 # Commit all changes in the current branch of the given repository with a WIP message
 github_commit_wip() {
   # Capture whether a directory was provided
@@ -36,37 +81,6 @@ github_commit_wip() {
   fi
 }
 
-# Get the current branch for a given path (default: current directory)
-github_current_branch() {
-  local file_path=${1:-$(pwd)}
-  git -C "$file_path" symbolic-ref --short HEAD
-}
-
-# Get the default branch based on the remote's HEAD
-github_default_branch() {
-  local file_path=${1:-$(pwd)}
-  git -C "$file_path" symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@'
-}
-
-# Get the GitHub repository URL in HTTPS format, removing any custom SSH host aliases
-github_project_root() {
-  local file_path=${1:-$(pwd)}
-  local url=$(git -C "$file_path" config remote.origin.url)
-
-  # Ensure the URL is non-empty
-  if [[ -z "$url" ]]; then
-    echo "Error: Unable to determine GitHub project root." >&2
-    return 1
-  fi
-
-  # Escape special characters and transform URL
-  echo "$url" |
-    sed -E 's|git@([^:]+):(.+)|https://\1/\2|' | # Convert SSH to HTTPS
-    sed -E 's|\.git$||' |                        # Remove `.git` suffix
-    sed -E 's|github\.com-work|github.com|' |    # Normalize host alias
-    sed -E 's|github\.com-personal|github.com|'  # Normalize host alias
-}
-
 # Open the GitHub compare page between the current branch and default branch in the browser
 github_compare_current_branch() {
   local file_path=${1:-$(pwd)}
@@ -75,73 +89,6 @@ github_compare_current_branch() {
   local default_branch=$(github_default_branch "$file_path")
 
   ${open_cmd} "${project_root}/compare/${default_branch}...${current_branch}?expand=1"
-}
-
-# Open the current GitHub repository in the default browser
-github_open_current_repo() {
-  local file_path=${1:-$(pwd)}
-  local project_root=$(github_project_root "$file_path")
-
-  if [[ -n "$project_root" ]]; then
-    ${open_cmd} "${project_root}"
-  else
-    echo "Error: Unable to open GitHub project root." >&2
-    return 1
-  fi
-}
-
-# Open the current branch's view of a specific file or directory in GitHub
-github_open_current_branch() {
-  local file=${1:-""}
-  local file_path=${2:-$(pwd)}
-  local current_branch=$(github_current_branch "$file_path")
-  local project_root=$(github_project_root "$file_path")
-  local git_directory=$(git -C "$file_path" rev-parse --show-prefix)
-
-  ${open_cmd} "${project_root}/tree/${current_branch}/${git_directory}${file}"
-}
-
-# Update the current branch by rebasing it on top of the default branch
-github_update_current_branch() {
-  local file_path=${1:-$(pwd)}
-  local default_branch=$(github_default_branch "$file_path")
-
-  git -C "$file_path" fetch origin
-  git -C "$file_path" pull origin "$default_branch" --rebase
-  git -C "$file_path" push origin "$(github_current_branch "$file_path")" --force --no-verify
-}
-
-# Get the GitHub username and repository name in the format "user/repo"
-github_user_repo() {
-  local file_path=${1:-$(pwd)}
-  git -C "$file_path" remote get-url origin |
-    sed -E 's|.*[:/]([^/]+/[^/]+)\.git$|\1|; s|.*[:/]([^/]+/[^/]+)$|\1|'
-}
-
-# Force commit all changes in the current branch of the given repository
-github_force_commit_changes() {
-  local file_path=${1:-$(pwd)}
-
-  # Add a timestamp to the commit message
-  local timestamp
-  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  local commit_message="[AUTOMATED] feat: misc changes - $timestamp"
-
-  # Change to file_path directory
-  pushd "$file_path" >/dev/null || {
-    echo "Error: Could not access directory."
-    return 1
-  }
-
-  # Add and commit changes
-  git add .
-  git commit -m "$commit_message" || echo "No changes to commit in provided path."
-
-  # Ensure branch is up-to-date with remote & force push changes
-  github_update_current_branch "$file_path"
-
-  # Return to the original directory
-  popd >/dev/null || exit
 }
 
 # Copy the diff of the current pull request
@@ -190,6 +137,56 @@ github_copy_diff() {
   return 0
 }
 
+# Force commit all changes in the current branch of the given repository
+github_force_commit_changes() {
+  local file_path=${1:-$(pwd)}
+
+  # Add a timestamp to the commit message
+  local timestamp
+  timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+  local commit_message="[AUTOMATED] feat: misc changes - $timestamp"
+
+  # Change to file_path directory
+  pushd "$file_path" >/dev/null || {
+    echo "Error: Could not access directory."
+    return 1
+  }
+
+  # Add and commit changes
+  git add .
+  git commit -m "$commit_message" || echo "No changes to commit in provided path."
+
+  # Ensure branch is up-to-date with remote & force push changes
+  github_update_current_branch "$file_path"
+
+  # Return to the original directory
+  popd >/dev/null || exit
+}
+
+# Open the current branch's view of a specific file or directory in GitHub
+github_open_current_branch() {
+  local file=${1:-""}
+  local file_path=${2:-$(pwd)}
+  local current_branch=$(github_current_branch "$file_path")
+  local project_root=$(github_project_root "$file_path")
+  local git_directory=$(git -C "$file_path" rev-parse --show-prefix)
+
+  ${open_cmd} "${project_root}/tree/${current_branch}/${git_directory}${file}"
+}
+
+# Open the current GitHub repository in the default browser
+github_open_current_repo() {
+  local file_path=${1:-$(pwd)}
+  local project_root=$(github_project_root "$file_path")
+
+  if [[ -n "$project_root" ]]; then
+    ${open_cmd} "${project_root}"
+  else
+    echo "Error: Unable to open GitHub project root." >&2
+    return 1
+  fi
+}
+
 # Prune local branches that are tracking deleted remotes
 github_prune_local_branches() {
   git fetch --prune
@@ -223,4 +220,11 @@ github_prune_local_branches() {
   else
     echo "❌ No branches deleted."
   fi
+}
+
+# Get the GitHub username and repository name in the format "user/repo"
+github_user_repo() {
+  local file_path=${1:-$(pwd)}
+  git -C "$file_path" remote get-url origin |
+    sed -E 's|.*[:/]([^/]+/[^/]+)\.git$|\1|; s|.*[:/]([^/]+/[^/]+)$|\1|'
 }
