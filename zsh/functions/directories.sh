9e77b1166c99939d52d7c36661975e95360f0b97
Jacob Penner <japenner@gmail.com>
2025-07-25 10:27:16 -0400

    [AUTOMATED] feat: misc changes to dotfiles - 2025-07-25 10:27:16

--- zsh/functions/directories.sh
+++ zsh/functions/directories.sh
@@ -7,10 +7,7 @@ function mcd() {
 
 # Function to go up multiple directories based on the number of dots used
 up() {
-  local count=${#1} # Get the number of dots in the argument
-  local path=""
-  for ((i = 1; i <= count; i++)); do
-    path+="../"
-  done
-  cd "$path" || return 1 # Navigate up, or return if the path is invalid
+  local count=${#1}
+  local path=$(printf '../%.0s' $(seq 1 $count))
+  cd "$path" || return 1
 }
